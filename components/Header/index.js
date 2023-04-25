c1761cdbd4cf7ebe133436dcd52cc20fa33f9520
Marco Gonzalez <magp2606@gmail.com>
2023-04-25 17:15:59 -0500

    added Header component

---
+++ components/Header/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+function Header({ text, children, styles }) {
+  return (
+    <>
+      <header style={styles}>
+        {children}
+        <h2>{text}</h2>
+      </header>
+
+      <style jsx>
+        {`
+          header {
+            position: sticky;
+            display: flex;
+            align-items: center;
+            height: 49px;
+            border-bottom: 1px solid #eee;
+            top: 0;
+            width: 100%;
+            background: #ffffffaa;
+            backdrop-filter: blur(5px);
+            padding: 16px;
+          }
+          h2 {
+            font-size: 20px;
+            font-weight: 800;
+            padding-left: 15px;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Header
