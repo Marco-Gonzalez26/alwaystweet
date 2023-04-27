581f5bc465b2e169c8d4d5e4ce914500e338db69
Marco Gonzalez <magp2606@gmail.com>
2023-04-27 14:37:25 -0500

    added header component to alweet page

--- pages/status/[id]/index.js
+++ pages/status/[id]/index.js
@@ -1,10 +1,11 @@
-import Alweet from "components/Alweet"
 import Head from "next/head"
+import { ArrowLeft } from "lucide-react"
+import { useRouter } from "next/router"
 
 import { getAlweet } from "../../../firebase/client"
 // import { firestore } from "../../../firebase/admin"
-import { useRouter } from "next/router"
-import ArrowLeft from "components/Icons/ArrowLeft"
+import Alweet from "components/Alweet"
+import Header from "components/Header"
 
 export default function AlweetPage(props) {
   const router = useRouter()
@@ -15,10 +16,10 @@ export default function AlweetPage(props) {
       <Head>
         <title>{props.userName && `Alweet of ${props.userName}`}</title>{" "}
       </Head>
-      <header>
+      <Header>
         <ArrowLeft onClick={() => router.push("/home")} />
         <h3>{props.userName && `Alweet of ${props.userName}`}</h3>
-      </header>
+      </Header>
       <Alweet {...props} />
       <style jsx>{`
         header {
@@ -30,7 +31,8 @@ export default function AlweetPage(props) {
           margin: 5px;
         }
         h3 {
-          font-size: 25px;
+          font-size: 1.25rem;
+          margin-left: 1rem;
         }
         :global(svg):hover {
           pointer-events: auto;
